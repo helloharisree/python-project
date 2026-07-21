cfc75b866c9ae0c56c81e1b617f08c653e2a3883
Hari <helloharisree@gmail.com>
2026-07-21 10:31:21 +0530

    Added user input program

---
+++ user.py
@@ -0,0 +1,11 @@
+Enter your name: Hari
+Enter your age: 25
+
+Hello, Hari
+You are 25 years old.
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+print("Hello,", name)
+print("You are", age, "years old.")
+
