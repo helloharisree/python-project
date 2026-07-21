14ae36853b35517490e1be5660ecfa72a6bcb775
Hari <helloharisree@gmail.com>
2026-07-21 12:57:07 +0530

    Added list examples

---
+++ list_example.py
@@ -0,0 +1,13 @@
+fruits = ["Apple", "Banana", "Mango", "Orange"]
+
+print("All fruits:")
+for fruit in fruits:
+    print(fruit)
+
+print("\nFirst fruit:", fruits[0])
+
+fruits.append("Grapes")
+
+print("\nUpdated list:")
+for fruit in fruits:
+    print(fruit)
